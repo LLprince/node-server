347b0a31d86775918fc37a16a0013b4ad0c1d6c4
Liu Tianyu <1270840370@qq.com>
2018-08-28 23:22:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 
 var server = http.createServer(function(req, res){
   try{
-    var fileContent = fs.readFileSync(__dirname + '/static' + req.url)
+    var fileContent = fs.readFileSync(__dirname + '/sample' + req.url)
     res.write(fileContent)
   }catch(e){
     res.writeHead(404, 'not found')
