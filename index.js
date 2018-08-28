1bb9c38ee2d55d7b847376067388b11ebd27d84f
Liu Tianyu <1270840370@qq.com>
2018-08-28 23:22:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+var http = require('http')
+var fs = require('fs')
+
+
+var server = http.createServer(function(req, res){
+  try{
+    var fileContent = fs.readFileSync(__dirname + '/static' + req.url)
+    res.write(fileContent)
+  }catch(e){
+    res.writeHead(404, 'not found')
+  }
+  res.end()
+})
+
+server.listen(8080)
+console.log('visit http://localhost:8080' )
